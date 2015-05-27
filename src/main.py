474d9f2d4632ef3010808f88bfc4457ce9dea247
Alexander Huntley <lgs12hunta@lesgrammar.org>
2015-05-27 11:20:08 +0000

    scan nfc for tags continually

--- src/main.py
+++ src/main.py
@@ -5,6 +5,18 @@ import nfc
 import signal
 import sys
 
+# Define cleanup function, used on exit
+def clean_up():
+    nfc.close(pnd)
+    nfc.exit(context)
+
+# Make sure ^C does not mess anything up
+def signal_handler(signal, frame):
+    clean_up()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler) 
+
 # Initialise NFC
 context = nfc.init()
 pnd = nfc.open(context)
@@ -20,7 +32,14 @@ if nfc.initiator_init(pnd) < 0:
 
 print('NFC reader %s opened' % nfc.device_get_name(pnd))
 
-# Clean up
+nm = nfc.modulation()
 
-nfc.close(pnd)
-nfc.exit(context)
+while True:
+    tag_number, tag = nfc.initiator_list_passive_targets(pnd, nm, 1)        # tag_number contains number of tags detected, tag contains their names
+    
+    if (tag_number > 0):
+        print("Detected tag...")
+        nfc.print_nfc_target(tag, verbose)
+
+# Clean up
+clean_up()
