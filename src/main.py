c44c2213ca584d423cda1aebd5176e0d73244bcd
Alexander Huntley <lgs12hunta@lesgrammar.org>
2015-05-29 15:55:57 +0000

    add comments

--- src/main.py
+++ src/main.py
@@ -40,9 +40,7 @@ nt = nfc.target()
 while True:
     # Wait for tag
     ret = nfc.initiator_select_passive_target(pnd, nmMifare, 0, 0, nt)
-    id = 1
-    if nt.nti.nai.abtUid[0] == 8:
-        id = 3
+    # Convert raw uid to hex, then trim to length (2*nt.nti.nai.szUidLen)
     tag = ''.join(format(x, '02x') for x in nt.nti.nai.abtUid)[:2*nt.nti.nai.szUidLen]
     print('Found tag with UID of', tag)
     time.sleep(1)
