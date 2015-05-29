b8bdcfb4416788663ee56cc04cda72ebec929323
Alexander Huntley <lgs12hunta@lesgrammar.org>
2015-05-29 13:39:59 +0000

    actually make NFC work

--- src/main.py
+++ src/main.py
@@ -1,45 +1,51 @@
-#!/usr/bin/env python3
-# assurpid: Listen for nfc tags and check them against an SQL database
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import nfc
+import time
 import signal
 import sys
 
-# Define cleanup function, used on exit
 def clean_up():
     nfc.close(pnd)
     nfc.exit(context)
 
-# Make sure ^C does not mess anything up
 def signal_handler(signal, frame):
     clean_up()
     sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler) 
+signal.signal(signal.SIGINT, signal_handler)
 
-# Initialise NFC
+print('Version: ', nfc.__version__)
+
 context = nfc.init()
 pnd = nfc.open(context)
-
 if pnd is None:
-    print('ERROR: unable to open NFC device (this program requires root privileges).')
+    print('ERROR: Unable to open NFC device.')
     exit()
 
 if nfc.initiator_init(pnd) < 0:
     nfc.perror(pnd, "nfc_initiator_init")
-    print('ERROR: unable to init NFC device')
+    print('ERROR: Unable to init NFC device.')
     exit()
 
-print('NFC reader %s opened' % nfc.device_get_name(pnd))
+print('NFC reader: %s opened' % nfc.device_get_name(pnd))
 
-nm = nfc.modulation()
+nmMifare = nfc.modulation()
+nmMifare.nmt = nfc.NMT_ISO14443A
+nmMifare.nbr = nfc.NBR_106
+
+nt = nfc.target()
 
 while True:
-    tag_number, tag = nfc.initiator_list_passive_targets(pnd, nm, 1)        # tag_number contains number of tags detected, tag contains their names
-    
-    if (tag_number > 0):
-        print("Detected tag...")
-        nfc.print_nfc_target(tag, verbose)
+    ret = nfc.initiator_select_passive_target(pnd, nmMifare, 0, 0, nt)
+    print('The following (NFC) ISO14443A tag was found:')
+    id = 1
+    if nt.nti.nai.abtUid[0] == 8:
+        id = 3
+    print('       UID (NFCID%d): ' % id , end='')
+    nfc.print_hex(nt.nti.nai.abtUid, nt.nti.nai.szUidLen)
+    time.sleep(1)
 
-# Clean up
-clean_up()
+nfc.close(pnd)
+nfc.exit(context)
