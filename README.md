75bfad05a45d681f710e38d81075cfa480d12c38
Alexander Huntley <lgs12hunta@lesgrammar.org>
2015-05-30 16:23:09 +0000

    Add systemd support

--- README.md
+++ README.md
@@ -8,8 +8,17 @@ A Raspberry Pi based prototype, written in Python. It is released under the BSD 
 * [SQLite](https://www.sqlite.org)
 
 ## Instructions
-1. Firstly run [src/createdb.py](src/createdb.py) as root. This will create the database. Add at least one admin tag, add as many as you like. They will be added to the database.
-2. Then you can run [src/main.py](src/main.py) as root. This will run main tag scanning loop.
-3. To add tags, scan an admin tag, then the tag you would like to add. To remove a tag, scan an admin tag followed by the one you want to remove.
-4. If you want to read the current database entries, run [src/catdb.py](src/catdb.py). It will print them all out.
+1. Wire up the NFC as described [here](https://www.adrive.com/public/Tw4su6/NFC_PN532.pdf)
+1. To enable SPI, required for NFC, copy [confix.txt](config.txt) to `/boot`, then reboot.
+1. Then run [src/createdb.py](src/createdb.py) as root. This will create the database. Add at least one admin tag, add as many as you like. They will be added to the database.
+1. Then you can run [src/main.py](src/main.py) as root. This will run main tag scanning loop.
+1. To add tags, scan an admin tag, then the tag you would like to add. To remove a tag, scan an admin tag followed by the one you want to remove.
+1. If you want to read the current database entries, run [src/catdb.py](src/catdb.py). It will print them all out.
 
+## Using systemd
+To make use of systemd for service startup and logging:
+1. Link main.py to /usr/local/bin/assurpid.py (`ln -s /path/to/assurpid/src/main.py /usr/local/bin/assurpid.py`)
+1. Copy `assurpid.service` to `/etc/systemd/system/`
+1. Reload systemd (`systemctl daemon-reload`)
+1. Start the service with `systemctl start assurpid`. You can start it on boot with `systemctl enable assurpid`.
+1. To read journal entries (output) run `journalctl -u assurpid`.
--- src/main.py
+++ src/main.py
@@ -7,6 +7,11 @@ import signal
 import sys
 import sqlite3
 import RPi.GPIO as GPIO
+import syslog
+
+def output(message):
+    print(message)
+    syslog.syslog(message)
 
 def light_on(colour):
     colours = {'red': [1,0,0], 'green': [0,1,0], 'blue': [0,0,1],\
@@ -24,7 +29,7 @@ def light_on(colour):
         GPIO.output(pins['green'], colours[colour][1])
         GPIO.output(pins['blue'], colours[colour][2])
     else:
-        print("Not a valid colour.")
+        output("Not a valid colour.")
 
 def clean_up():
     nfc.close(pnd)
@@ -41,22 +46,22 @@ sleep_time = 2
 signal.signal(signal.SIGINT, signal_handler)
 
 conn = sqlite3.connect('/var/db/assurpid/assurpid.db')
-print('opened database successfully')
+output('opened database successfully')
 
-print('libnfc version: ', nfc.__version__)
+output('libnfc version: ', nfc.__version__)
 
 context = nfc.init()
 pnd = nfc.open(context)
 if pnd is None:
-    print('ERROR: Unable to detect RFID sensor (run this as root).')
+    output('ERROR: Unable to detect RFID sensor (run this as root).')
     exit()
 
 if nfc.initiator_init(pnd) < 0:
     nfc.perror(pnd, "nfc_initiator_init")
-    print('ERROR: Unable to init RFID sensor.')
+    output('ERROR: Unable to init RFID sensor.')
     exit()
 
-print('NFC reader: %s opened' % nfc.device_get_name(pnd))
+output('NFC reader: %s opened' % nfc.device_get_name(pnd))
 
 nmMifare = nfc.modulation()
 nmMifare.nmt = nfc.NMT_ISO14443A
@@ -70,66 +75,66 @@ while True:
     if not admin_switch:
         GPIO.cleanup()
 
-    print()
+    output()
 
     # Wait for tag
     ret = nfc.initiator_select_passive_target(pnd, nmMifare, 0, 0, nt)
 
     # Convert raw uid to hex, then trim to length (2*nt.nti.nai.szUidLen)
     tag = ''.join(format(x, '02x') for x in nt.nti.nai.abtUid)[:2*nt.nti.nai.szUidLen]
-    print('Found tag with UID of', tag)
+    output('Found tag with UID of', tag)
     cursor = conn.cursor()
     cursor.execute("SELECT UID, ADMIN, ACCESS_ROOM1, INSIDE_ROOM1 FROM MAIN WHERE UID = ?", (tag,))
     details = cursor.fetchone()
 
     if details is None:
-        print("Tag is unrecognised")
+        output("Tag is unrecognised")
         light_on('red')
         if admin_switch:
             conn.execute("INSERT INTO MAIN(UID, ADMIN, ACCESS_ROOM1, INSIDE_ROOM1) VALUES(?, 0, 1, 0)", (tag,))
             conn.commit()
             admin_switch = False
             light_on('green')
-            print("Added tag to database")
+            output("Added tag to database")
         time.sleep(sleep_time)
         continue
     else:
-        print("Tag is recognised")
+        output("Tag is recognised")
         if admin_switch and details[1] == 0:
             conn.execute("DELETE FROM MAIN WHERE UID = ?", (tag,))
             conn.commit()
             admin_switch = False
             light_on('red')
-            print("Deleted tag from database")
+            output("Deleted tag from database")
             time.sleep(sleep_time)
             continue
 
     if details[1] == 1:
-        print("Tag is admin.")
+        output("Tag is admin.")
         light_on('magenta')
         admin_switch = True
         time.sleep(sleep_time)
         continue
 
     if details[3] == 1:
-        print("Tag is inside Room 1.")
+        output("Tag is inside Room 1.")
     else:
-        print("Tag is outside Room 1.")
+        output("Tag is outside Room 1.")
 
     if details[2] == 1:
-        print("Tag can open door to Room 1.")
+        output("Tag can open door to Room 1.")
         light_on('green')
     else:
-        print("Tag cannot open door to Room 1.")
+        output("Tag cannot open door to Room 1.")
         light_on('blue')
     
     if details[2] == 1 and details[3]:
         conn.execute("UPDATE MAIN SET INSIDE_ROOM1 = 0 WHERE UID = ?", (tag,))
-        print("Door opened from inside, tag now outside.")
+        output("Door opened from inside, tag now outside.")
     
     if details[2] == 1 and details[3] == 0:
         conn.execute("UPDATE MAIN SET INSIDE_ROOM1 = 1 WHERE UID = ?", (tag,))
-        print("Door opened from outside, tag now inside.")
+        output("Door opened from outside, tag now inside.")
     time.sleep(sleep_time)
 
 clean_up()
